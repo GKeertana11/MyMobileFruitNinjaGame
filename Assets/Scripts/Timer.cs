095fa61a2cf12c65cdd598ef273eca471683f384
KeertanaG <keertana.govte@yesgnome.com>
2022-05-19 13:59:18 +0530

    Assets added.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,44 +6,43 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     #region PUBLIC VARIABLES
-    
-    public GameObject alertReference;
+    public float time;
+    public int timescore = 60;
+    public GameObject Gameover;
     #endregion
+
     #region PRIVATE VARIABLES
-    private Text timeTF;
+    private Text timetext;
     #endregion
 
     #region MONOBEHAVIOUR BETHODS
     // Start is called before the first frame update
     void Start()
     {
-        timeTF = gameObject.GetComponent<Text>();
-        InvokeRepeating("ReduceTime", 1, 1);
+        timetext= gameObject.GetComponent<Text>();
+       // InvokeRepeating("ReduceTime", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    #endregion
-    void ReduceTime()
-    {
-        if (timeTF.text == "1")
+        time = time + Time.deltaTime;
+        if (time >= 1f)
         {
-            /* Alert */
+            timescore--;
 
-            Time.timeScale = 0;
-           // Instantiate(alertReference, new Vector3(0.5f, 0.5f, 0), transform.rotation);
-            //audio.Play();
-            // GameObject.Find("AppleGUI").GetComponent<AudioSource>().Stop();
-            alertReference.SetActive(true);
+            if (timescore == 0)
+            {
+                Gameover.SetActive(true);
+            }
+            else
+            {
+                timetext.text = timescore.ToString();
+                time = 0f;
+            }
         }
+    }
 
-       timeTF.text = (int.Parse(timeTF.text) - 1).ToString();
-    }
-    void Reload()
-    {
-        Application.LoadLevel(Application.loadedLevel);
-    }
+    #endregion
 }
+  
